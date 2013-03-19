231cd6c70afe72beebd832c6545a218647aa0639
Takuma Nakajima <penguin.trance.2716@gmail.com>
2013-03-20 03:02:34 +0900

    change class variables to instance variables

--- mikutter_teokure_tweet.rb
+++ mikutter_teokure_tweet.rb
@@ -1,22 +1,22 @@
 #-*- coding: utf-8 -*-
 Plugin.create :teokure do
 
-  @@t = "tT" + "ｔＴ" + "ТтҬҭԎԏ" + "ȚțŤťT̈ẗṪŦƬȾ"
-  @@e = "eE" + "ｅＥ" + "ЀЁЄЕЗезѐёєѮѯҘҙҨҩҼҽҾҿӖӗӘәӚӛӞӟԐԑЀЁ" + "∃Ǝ℮" + "ÈèÉéÊêËëĒēĖėĘęĚěƏə"
-  @@o = "oO" + "ｏＯ" + "ОФофѲѳѺѻ҈҉ӦӧӨөӪӫ" + "∅ℚ" + "ÒòÓóÔôÕõÖöØøŐőƠơǪǫŌōǬǭƟɵº" + "〄"
-  @@k = "kK" + "ｋＫ" + "ЌКкҚқҜҝҞҟҠҡӃӄԞԟ" + "ĶḰǨḲḴƘ₭"
-  @@u = "uU" + "ｕＵ" + "ЏЦЧцчџѴѵѶѷҴҵҶҷҸҹӋӌӮӯӰӱӲӳӴӵ" + "∪" + "ÙùÚúÛûÜüŪūŬŭŮůŰűŲųƯưŨũÜü"
-  @@r = "rR" + "ｒＲ" + "ЯяԄԅ" + "ℝℜ" + "ŘřŔŕṘŖȐɌȒṚṜṞ"
-  @@te_ = "てテﾃ" + "〒"
-  @@o_  = "おぉオｵォｫ" + ""
-  @@ku_ = "くクｸ" + "<＜〈"
-  @@re_ = "れレﾚ" + "Ԙԙ" + ""
+  @t = "tT" + "ｔＴ" + "ТтҬҭԎԏ" + "ȚțŤťT̈ẗṪŦƬȾ"
+  @e = "eE" + "ｅＥ" + "ЀЁЄЕЗезѐёєѮѯҘҙҨҩҼҽҾҿӖӗӘәӚӛӞӟԐԑЀЁ" + "∃Ǝ℮" + "ÈèÉéÊêËëĒēĖėĘęĚěƏə"
+  @o = "oO" + "ｏＯ" + "ОФофѲѳѺѻ҈҉ӦӧӨөӪӫ" + "∅ℚ" + "ÒòÓóÔôÕõÖöØøŐőƠơǪǫŌōǬǭƟɵº" + "〄"
+  @k = "kK" + "ｋＫ" + "ЌКкҚқҜҝҞҟҠҡӃӄԞԟ" + "ĶḰǨḲḴƘ₭"
+  @u = "uU" + "ｕＵ" + "ЏЦЧцчџѴѵѶѷҴҵҶҷҸҹӋӌӮӯӰӱӲӳӴӵ" + "∪" + "ÙùÚúÛûÜüŪūŬŭŮůŰűŲųƯưŨũÜü"
+  @r = "rR" + "ｒＲ" + "ЯяԄԅ" + "ℝℜ" + "ŘřŔŕṘŖȐɌȒṚṜṞ"
+  @te_ = "てテﾃ" + "〒"
+  @o_  = "おぉオｵォｫ" + ""
+  @ku_ = "くクｸ" + "<＜〈"
+  @re_ = "れレﾚ" + "Ԙԙ" + ""
 
-  @@re = /((?:[#{@@t}]\s*[#{@@e}]|[#{@@te_}])\s*(?:[#{@@o}]|[#{@@o_}])\s*(?:[#{@@k}]\s*[#{@@u}]|[#{@@ku_}])\s*(?:[#{@@r}]\s*[#{@@e}]|[#{@@re_}]))/
+  @re = /((?:[#{@t}]\s*[#{@e}]|[#{@te_}])\s*(?:[#{@o}]|[#{@o_}])\s*(?:[#{@k}]\s*[#{@u}]|[#{@ku_}])\s*(?:[#{@r}]\s*[#{@e}]|[#{@re_}]))/
 
   on_appear do |ms|
     ms.each do |m|
-      if m.message.to_s =~ @@re and m.user != Service.primary.user
+      if m.message.to_s =~ @re and m.user != Service.primary.user
         m.favorite
       end
     end
@@ -28,13 +28,13 @@ Plugin.create :teokure do
           visible: true,
           role: :postbox) do |opt|
     begin
-      teokure  = @@t.split('')[rand(@@t.size)]
-      teokure += @@e.split('')[rand(@@e.size)]
-      teokure += @@o.split('')[rand(@@o.size)]
-      teokure += @@k.split('')[rand(@@k.size)]
-      teokure += @@u.split('')[rand(@@u.size)]
-      teokure += @@r.split('')[rand(@@r.size)]
-      teokure += @@e.split('')[rand(@@e.size)]
+      teokure  = @t.split('')[rand(@t.size)]
+      teokure += @e.split('')[rand(@e.size)]
+      teokure += @o.split('')[rand(@o.size)]
+      teokure += @k.split('')[rand(@k.size)]
+      teokure += @u.split('')[rand(@u.size)]
+      teokure += @r.split('')[rand(@r.size)]
+      teokure += @e.split('')[rand(@e.size)]
       Service.primary.post :message => teokure
     rescue Exception => e
       Plugin.call(:update, nil, [Message.new(:message => "#{e.to_s}", :system => true)])
